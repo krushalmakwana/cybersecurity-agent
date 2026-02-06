b791d7d1133020dc0dd6d3ee42c842771b7dec29
krushalmakwana <krushalmakwana08@gmail.com>
2026-02-06 19:38:29 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,15 +1,32 @@
-from detector import detect_bruteforce
-from config import LOG_FILE
+def cybersecurity_bot():
+    print("Cybersecurity Agent is running...")
+    print("Ask me a cybersecurity question.")
+    print("Type 'exit' to stop.\n")
 
-def load_logs():
-    with open(LOG_FILE, "r") as file:
-        return file.readlines()
+    while True:
+        question = input("You: ").lower()
 
-def run_agent():
-    print("Cybersecurity Agent Started...\n")
-    logs = load_logs()
-    detect_bruteforce(logs)
-    print("\nMonitoring completed.")
+        if question == "exit":
+            print("Agent stopped.")
+            break
+
+        elif "phishing" in question:
+            print("Agent: Phishing is a cyber attack where attackers trick users into revealing sensitive information.")
+
+        elif "malware" in question:
+            print("Agent: Malware is malicious software designed to damage or gain unauthorized access to systems.")
+
+        elif "firewall" in question:
+            print("Agent: A firewall is a security system that monitors and controls incoming and outgoing network traffic.")
+
+        elif "brute force" in question:
+            print("Agent: Brute force attack attempts to guess passwords by trying many combinations.")
+
+        elif "vpn" in question:
+            print("Agent: VPN creates a secure encrypted connection over the internet.")
+
+        else:
+            print("Agent: Sorry, I don't know the answer yet.")
 
 if __name__ == "__main__":
-    run_agent()
+    cybersecurity_bot()
